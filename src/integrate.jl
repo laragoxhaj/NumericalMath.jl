78d5e2c4fd35a033cfd7a88ee05d1b8159be46bc
LaraGoxhaj <laragoxhaj@gmail.com>
2016-04-09 15:29:02 -0400

    clean

--- src/integrate.jl
+++ src/integrate.jl
@@ -33,28 +33,28 @@ end
 =#
 function atrapz{Tx<:Number, Ty<:Number}(x::Array{Tx}, y::Array{Ty}, absv::Bool)
 	
-	local m = size(x,1)
-	local n = size(x,2)
-	if (size(y,1) != m && size(y,2) != n)
-		error("Arrays 'x', 'y' must be of the same dimensions")
-	end
+    local m = size(x,1)
+    local n = size(x,2)
+    if (size(y,1) != m && size(y,2) != n)
+	error("Arrays 'x', 'y' must be of the same dimensions")
+    end
 
-	r = zeros(1, n);
-	for k in 2:m
-		r[1,:] += (x[k,:] - x[k-1,:]) .* (y[k,:] + y[k-1,:]);
-	end
-	if (absv)
-		r = abs(r);
-	end
+    r = zeros(1, n);
+    for k in 2:m
+	r[1,:] += (x[k,:] - x[k-1,:]) .* (y[k,:] + y[k-1,:]);
+    end
+    if (absv)
+	r = abs(r);
+    end
 
-	# error -h^2/12 * (f'(b) - f'(a))	
-	ha = x[2,:] - x[1,:];
-	he = x[end,:] - x[end-1,:];
-	ra = (y[2,:] - y[1,:]) ./ ha;
-	re = (y[end,:] - y[end-1,:]) ./ he;
-	err = abs(ha .* he ./ 12 .* (re - ra));
+    # error -h^2/12 * (f'(b) - f'(a))	
+    ha = x[2,:] - x[1,:];
+    he = x[end,:] - x[end-1,:];
+    ra = (y[2,:] - y[1,:]) ./ ha;
+    re = (y[end,:] - y[end-1,:]) ./ he;
+    err = abs(ha .* he ./ 12 .* (re - ra));
 
-	return [r./2; err];
+    return [r./2; err];
 end
 
 
